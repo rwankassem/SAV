424255b3b684f43233cdf95d57c7cf0051d50e50
Rwan Kassem <rwankassem68@gmail.com>
2026-04-18 20:00:59 +0200

    project update

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import latest_status_view, alerts_view
-
-urlpatterns = [
-    path("status/", latest_status_view),
-    path("alerts/", alerts_view),
-]
--- django_backend_draft/drowsiness_system/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from api.views import home_view
-
-urlpatterns = [
-    path("", home_view),
-    path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
-]
