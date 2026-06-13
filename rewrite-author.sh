f48d7cf276fb8f2cc6eb5fbb1d571d24374c3ea5
Rwan Taha <118690761+rwankassem@users.noreply.github.com>
2026-06-13 16:59:10 +0300

    update

--- rewrite-author.sh
+++
@@ -1,8 +0,0 @@
-if [ "$GIT_AUTHOR_EMAIL" = "mosayyyed1@github.com" ]; then
-  export GIT_AUTHOR_NAME="Rwan Kassem"
-  export GIT_AUTHOR_EMAIL="rwankassem68@gmail.com"
-fi
-if [ "$GIT_COMMITTER_EMAIL" = "mosayyyed1@github.com" ]; then
-  export GIT_COMMITTER_NAME="Rwan Kassem"
-  export GIT_COMMITTER_EMAIL="rwankassem68@gmail.com"
-fi
