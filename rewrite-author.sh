f30c98bcbd36d4565149b92fc347aaeb3f1d61f1
rwan <rwankassem68@gmail.com>
2026-06-13 16:53:06 +0300

    update

---
+++ rewrite-author.sh
@@ -0,0 +1,8 @@
+if [ "$GIT_AUTHOR_EMAIL" = "mosayyyed1@github.com" ]; then
+  export GIT_AUTHOR_NAME="Rwan Kassem"
+  export GIT_AUTHOR_EMAIL="rwankassem68@gmail.com"
+fi
+if [ "$GIT_COMMITTER_EMAIL" = "mosayyyed1@github.com" ]; then
+  export GIT_COMMITTER_NAME="Rwan Kassem"
+  export GIT_COMMITTER_EMAIL="rwankassem68@gmail.com"
+fi
